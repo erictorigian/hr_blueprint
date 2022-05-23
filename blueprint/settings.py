41470b488cba5b4e1ab0e380212761c5ebf06db3
Eric Torigian <eric@torigian.com>
2022-05-23 14:28:23 -0400

    tweaked config

--- blueprint/settings.py
+++ blueprint/settings.py
@@ -8,9 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gz&p=2l1k(axfwcct*$j6^mrbub6o)ga9$=^^#ovj&!@obt5w8'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
